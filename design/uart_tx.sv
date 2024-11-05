a493f21caef159d537ecc9c13649a6eeaa1d88bc
will <artcloudcn@163.com>
2024-11-05 16:08:02 +0800

    发送增加停止位

--- design/uart_tx.sv
+++ design/uart_tx.sv
@@ -15,10 +15,10 @@ module uart_tx #(
  
     logic   [$clog2(BIT_PERIOD)-1:0]    baud_cnt    ;
     logic   [$clog2(DATA_WIDTH):0]      bit_cnt     ;            //bit_cnt计数为数据位+起始位，所以位宽要加1
-    logic   [DATA_WIDTH:0]              tx_data     ;            //串口的发送数据：数据位+起始位
+    logic   [DATA_WIDTH+1:0]            tx_data     ;            //串口的发送数据：数据位+起始位
     logic                               tx_finish   ;
 
-    assign tx_finish = (bit_cnt == DATA_WIDTH)&&(baud_cnt == BIT_PERIOD - 1) ? 1'b1 : 1'b0;  //因为串口连续发送数据可以不用停止位，所以计数到9即可表示一次发送结束
+    assign tx_finish = (bit_cnt == DATA_WIDTH + 1)&&(baud_cnt == BIT_PERIOD - 1) ? 1'b1 : 1'b0;  //因为串口连续发送数据可以不用空闲位，所以计数到DATA_WIDTH+2即可表示一次发送结束
     always_ff @( posedge clk or posedge rst ) begin
         if (rst) begin
            s_axis_tready    <=  1'b1; 
@@ -26,10 +26,10 @@ module uart_tx #(
         end
         else if (s_axis_tvalid&&s_axis_tready) begin        //握手成功
             s_axis_tready   <=  1'b0;
-            tx_data         <=  {s_axis_tdata,1'b0};
+            tx_data         <=  {1'b1,s_axis_tdata,1'b0};               //添加起始位和停止位
         end
         else begin
-            s_axis_tready   <=  tx_finish?     ~s_axis_tready : s_axis_tready;
+            s_axis_tready   <=  tx_finish?     1'b1 : s_axis_tready;    
             tx_data         <=  (baud_cnt == BIT_PERIOD - 1)?   tx_data >> 1 : tx_data;
         end
     end
